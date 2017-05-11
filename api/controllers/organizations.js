51f919e9d1328016e7862dfc0a60d16308e249a6
Timothy Kevin Brown <kevin@ki-ai.org>
2017-05-11 16:09:51 -0500

    add subdocs to organizations

--- api/controllers/organizations.js
+++ api/controllers/organizations.js
@@ -1,33 +1,178 @@
-'use strict';
-
 const db = require('../db');
 const organizations = db.get('organizations', { castIds: false });
+const contracts = db.get('contracts', { castIds: false });
+const q2m = require('query-to-mongo');
+const qs = require('qs');
+const omit = require('lodash/omit');
+const pickBy = require('lodash/pickBy');
+const mapValues = require('lodash/mapValues');
+const omitEmpty = require('./lib').omitEmpty;
 
-module.exports = {
-  allOrganizations: allOrganizations,
-  singleOrganization: singleOrganization,
+function orgDataMap(array) {
+  return array.map(o => {
+    const regex = new RegExp(o.simple, 'i');
+    const contractsP = contracts.find({ dependency: regex });
+
+    const object = o;
+    object.shares = o.memberships_org.filter(b => (b.role === 'shareholder'));
+    object.board = o.memberships_sob.filter(b => (b.department === 'board'));
+    object.shareholders = o.memberships_sob.filter(b => (b.role === 'shareholder'));
+    object.memberships = o.memberships_sob.filter(b => {
+      return (b.role !== 'shareholder' && b.department !== 'board');
+    });
+
+    return omitEmpty(omit(object, ['memberships_org', 'memberships_sob']));
+  });
+}
+
+const defaultProjection = {
+  user_id: 0,
+  'suppliesContracts.user_id': 0,
+  'memberships_sob._id': 0,
+  'memberships_org._id': 0,
+  'memberships_sob.user_id': 0,
+  'memberships_sob.sob_org': 0,
+  'memberships_org.user_id': 0,
 };
 
+function queryToPipeline(query) {
+  let projection = defaultProjection;
+
+  if (query.options.fields) {
+    projection = query.options.fields;
+  }
+  const pipeline = [
+    { $match: query.criteria },
+    {
+      $lookup: {
+        from: 'contracts',
+        localField: 'simple',
+        foreignField: 'suppliers_org',
+        as: 'suppliesContracts',
+      },
+    },
+    {
+      $lookup: {
+        from: 'memberships',
+        localField: 'simple',
+        foreignField: 'org_id',
+        as: 'memberships_org',
+      },
+    },
+    {
+      $lookup: {
+        from: 'memberships',
+        localField: 'simple',
+        foreignField: 'sob_org',
+        as: 'memberships_sob',
+      },
+    },
+  ];
+
+  if (query.options.skip) {
+    pipeline.push({ $skip: query.options.skip });
+  }
+  if (query.options.limit) {
+    pipeline.push({ $limit: query.options.limit });
+  }
+
+  if (query.options.sort) {
+    pipeline.push({ $sort: query.options.sort });
+  }
+
+  pipeline.push({ $project: projection });
+  return pipeline;
+}
+
 function allOrganizations(req, res, next) {
-  organizations.find({}, { limit: 10 }).then(docs => {
-    res.json({
-        status: 'success',
-        data: docs,
-        size: docs.length,
-        message: 'Retrieved ALL persons',
+  const params = pickBy(req.swagger.params, p => (p.value));
+  const mapped = mapValues(params, p => (p.value));
+  const string = qs.stringify(mapped);
+  const query = q2m(string);
+  const pipeline = queryToPipeline(query);
+
+  organizations.aggregate(pipeline).then(docs => {
+    const data = orgDataMap(docs);
+
+    const PP = data.map(o => {
+      const regex = new RegExp(o.names.join('|'), 'i');
+
+      return new Promise((resolve, reject) => {
+        contracts.find({ dependency: regex }, '-user_id').then(c => {
+          o.contracts = c;
+          resolve(o);
+        });
       });
+    });
+
+    Promise.all(PP).then(values => {
+      res.charSet('utf-8');
+      res.json({
+        status: 'success',
+        data: values,
+        size: values.length,
+      });
+    });
+
   });
 }
 
 function singleOrganization(req, res) {
-  // variables defined in the Swagger document can be referenced using req.swagger.params.{parameter_name}
-  var id = req.swagger.params._id.value;
-  organizations.find({ _id: id }).then(docs => {
-    res.json({
-        status: 'success',
-        data: docs,
-        message: 'Retrieved person',
+  const id = req.swagger.params._id.value;
+
+  organizations.aggregate([
+    { $match: { _id: id } },
+    {
+      $lookup: {
+        from: 'contracts',
+        localField: 'simple',
+        foreignField: 'suppliers_org',
+        as: 'suppliesContracts',
+      },
+    },
+    {
+      $lookup: {
+        from: 'memberships',
+        localField: 'simple',
+        foreignField: 'org_id',
+        as: 'memberships_org',
+      },
+    },
+    {
+      $lookup: {
+        from: 'memberships',
+        localField: 'simple',
+        foreignField: 'sob_org',
+        as: 'memberships_sob',
+      },
+    },
+    { $project: defaultProjection },
+  ]).then(docs => {
+    const data = orgDataMap(docs);
+
+    const PP = data.map(o => {
+      const regex = new RegExp(o.names.join('|'), 'i');
+
+      return new Promise((resolve, reject) => {
+        contracts.find({ dependency: regex }, '-user_id').then(c => {
+          o.contracts = c;
+          resolve(o);
+        });
       });
+    });
+
+    Promise.all(PP).then(values => {
+      res.charSet('utf-8');
+      res.json({
+        status: 'success',
+        data: values,
+        size: values.length,
+      });
+    });
   });
-  // this sends back a JSON response which is a single string
 }
+
+module.exports = {
+  allOrganizations,
+  singleOrganization,
+};
