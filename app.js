0e05b4a020e46ae6d76713fc098fd609f9240ef6
Timothy Kevin Brown <kevin@ki-ai.org>
2017-05-11 16:10:42 -0500

    cleanup app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const SwaggerRestify = require('swagger-restify-mw');
 const restify = require('restify');
 const app = restify.createServer();
@@ -8,12 +6,15 @@ const config = {
   appRoot: __dirname, // required config
 };
 
-SwaggerRestify.create(config, function(err, swaggerRestify) {
-  if (err) { throw err; }
+SwaggerRestify.create(config, (err, swaggerRestify) => {
+  if (err) {
+    throw err;
+  }
 
   swaggerRestify.register(app);
 
   const port = process.env.PORT || 10010;
+
   app.listen(port);
   if (swaggerRestify.runner.swagger.paths['/hello']) {
     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
