e9c123be71a3b2635611b9612dba866f361bbefa
Timothy Kevin Brown <kevin@ki-ai.org>
2017-05-11 13:27:01 -0500

    add some shared functions for controllers

---
+++ api/controllers/lib.js
@@ -0,0 +1,32 @@
+const flow = require('lodash/fp/flow');
+const omitBy = require('lodash/fp/omitBy');
+const isArray = require('lodash/fp/isArray');
+const isEmpty = require('lodash/fp/isEmpty');
+const isDate = require('lodash/fp/isDate');
+const isObject = require('lodash/fp/isObject');
+const isString = require('lodash/fp/isString');
+const isNil = require('lodash/fp/isNil');
+const extend = require('lodash/extend');
+const omit = require('lodash/omit');
+
+function personMemberMap(doc) {
+  const member = extend(doc, {
+    org_id: doc.sob_org,
+  });
+
+  return omit(member, ['sob_org']);
+}
+
+function omitEmpty(object) {
+  return flow(
+    omitBy(isNil),
+    omitBy(v => (isArray(v) && isEmpty(v))),
+    omitBy(v => (isString(v) && isEmpty(v))),
+    omitBy(v => (!isDate(v) && isObject(v) && isEmpty(v)))
+  )(object);
+}
+
+module.exports = {
+  personMemberMap,
+  omitEmpty,
+};
