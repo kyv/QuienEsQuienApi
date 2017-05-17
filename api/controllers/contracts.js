e0fc4cb592634f88a8667ccafcdd9114b213b20a
Timothy Kevin Brown <kevin@ki-ai.org>
2017-05-17 13:14:01 -0500

    update controllers

--- api/controllers/contracts.js
+++ api/controllers/contracts.js
@@ -1,27 +1,44 @@
 const db = require('../db');
 const contracts = db.get('contracts', { castIds: false });
-const organizations = db.get('organizations', { castIds: false });
 const q2m = require('query-to-mongo');
 const qs = require('qs');
 const omit = require('lodash/omit');
 const pickBy = require('lodash/pickBy');
 const mapValues = require('lodash/mapValues');
 const omitEmpty = require('./lib').omitEmpty;
-const simpleName = require('./lib').simpleName;
-
 
 function contractMapData(object) {
-  const data = omit(object, ['suppliersOrg', 'suppliersPerson', 'dependencyOrg']);
+  const data = omit(object, [
+    'user_id',
+    'suppliersOrg',
+    'suppliersPerson',
+    'dependencyOrg',
+    'suppliers_org',
+    'suppliers_person',
+  ]);
 
-  data.dependency = object.dependencyOrg;
-  data.suppliers_org = object.suppliersOrg;
-  data.suppliers_person = object.suppliersPerson;
+  if (object.suppliersOrg[0] && object.suppliersOrg[0].simple) {
+    data.suppliers_org = object.suppliersOrg;
+  }
+  if (object.suppliersPerson[0] && object.suppliersPerson[0].simple) {
+    data.suppliers_person = object.suppliersPerson;
+  }
   return omitEmpty(data);
 }
 
 function queryToPipeline(query) {
   const pipeline = [
     { $match: query.criteria },
+    { $unwind: {
+      path: '$suppliers_person',
+      preserveNullAndEmptyArrays: true,
+    },
+    },
+    { $unwind: {
+      path: '$suppliers_org',
+      preserveNullAndEmptyArrays: true,
+    },
+    },
     {
       $lookup: {
         from: 'organizations',
@@ -52,6 +69,7 @@ function queryToPipeline(query) {
   }
 
   // pipeline.push({ $project: projection });
+  // FIXME add in mongodb >= 3.4
   return pipeline;
 }
 
@@ -63,25 +81,18 @@ function allContracts(req, res, next) {
   const pipeline = queryToPipeline(query);
 
   contracts.aggregate(pipeline).then(docs => {
-    const PP = docs.map(o => {
-      const simple = simpleName(o.dependency);
-
-      return new Promise((resolve, reject) => {
-        organizations.find({ simple }, '-user_id').then(c => {
-          o.dependencyOrg = c;
-          resolve(o);
-        });
-      });
+    res.charSet('utf-8');
+    res.header('content-type', 'application/json');
+    return res.end({
+      status: 'success',
+      data: docs.map(o => contractMapData(o)),
+      size: docs.length,
     });
-
-    Promise.all(PP).then(values => {
-      res.charSet('utf-8');
-      res.json({
-        status: 'success',
-        data: values.map(o => contractMapData(o)),
-        size: values.length,
-      });
-    });
+    // res.json({
+    //   status: 'success',
+    //   data: docs.map(o => contractMapData(o)),
+    //   size: docs.length,
+    // });
   });
 }
 
@@ -90,6 +101,16 @@ function singleContract(req, res) {
 
   contracts.aggregate([
     { $match: { _id: id } },
+    { $unwind: {
+      path: '$suppliers_person',
+      preserveNullAndEmptyArrays: true,
+    },
+    },
+    { $unwind: {
+      path: '$suppliers_org',
+      preserveNullAndEmptyArrays: true,
+    },
+    },
     {
       $lookup: {
         from: 'organizations',
@@ -107,24 +128,11 @@ function singleContract(req, res) {
       },
     },
   ]).then(docs => {
-    const PP = docs.map(o => {
-      const simple = simpleName(o.dependency);
-
-      return new Promise((resolve, reject) => {
-        organizations.find({ simple }, '-user_id').then(c => {
-          o.dependencyOrg = c;
-          resolve(o);
-        });
-      });
-    });
-
-    Promise.all(PP).then(values => {
-      res.charSet('utf-8');
-      res.json({
-        status: 'success',
-        data: values.map(o => contractMapData(o)),
-        size: values.length,
-      });
+    res.charSet('utf-8');
+    res.json({
+      status: 'success',
+      data: docs.map(o => contractMapData(o)),
+      size: docs.length,
     });
   });
 }
--- api/controllers/organizations.js
+++ api/controllers/organizations.js
@@ -1,6 +1,5 @@
 const db = require('../db');
 const organizations = db.get('organizations', { castIds: false });
-const contracts = db.get('contracts', { castIds: false });
 const q2m = require('query-to-mongo');
 const qs = require('qs');
 const omit = require('lodash/omit');
@@ -8,39 +7,31 @@ const pickBy = require('lodash/pickBy');
 const mapValues = require('lodash/mapValues');
 const omitEmpty = require('./lib').omitEmpty;
 
-function orgDataMap(array) {
-  return array.map(o => {
-    const regex = new RegExp(o.simple, 'i');
-    const contractsP = contracts.find({ dependency: regex });
+function orgDataMap(o) {
+  const object = omit(o, ['memberships_sob', 'shares']);
+  const sob = o.memberships_sob.map(b => (omit(b, ['_id', 'user_id', 'sob_org'])));
 
-    const object = o;
-    object.shares = o.memberships_org.filter(b => (b.role === 'shareholder'));
-    object.board = o.memberships_sob.filter(b => (b.department === 'board'));
-    object.shareholders = o.memberships_sob.filter(b => (b.role === 'shareholder'));
-    object.memberships = o.memberships_sob.filter(b => {
-      return (b.role !== 'shareholder' && b.department !== 'board');
+  object.shares = o.shares
+    .map(b => {
+      b.org_id = b.sob_org;
+      return omit(b, ['_id', 'user_id', 'sob_org', 'org', 'role']);
     });
 
-    return omitEmpty(omit(object, ['memberships_org', 'memberships_sob']));
+  object.board = sob
+    .filter(b => (b.department === 'board'))
+    .map(b => (omit(b, ['department'])));
+
+  object.shareholders = sob.filter(b => (b.role === 'shareholder'))
+    .map(b => (omit(b, 'role')));
+
+  object.memberships = sob.filter(b => {
+    return (b.role !== 'shareholder' && b.department !== 'board');
   });
+
+  return omitEmpty(object);
 }
 
-const defaultProjection = {
-  user_id: 0,
-  'suppliesContracts.user_id': 0,
-  'memberships_sob._id': 0,
-  'memberships_org._id': 0,
-  'memberships_sob.user_id': 0,
-  'memberships_sob.sob_org': 0,
-  'memberships_org.user_id': 0,
-};
-
 function queryToPipeline(query) {
-  let projection = defaultProjection;
-
-  if (query.options.fields) {
-    projection = query.options.fields;
-  }
   const pipeline = [
     { $match: query.criteria },
     {
@@ -56,7 +47,7 @@ function queryToPipeline(query) {
         from: 'memberships',
         localField: 'simple',
         foreignField: 'org_id',
-        as: 'memberships_org',
+        as: 'shares',
       },
     },
     {
@@ -80,7 +71,8 @@ function queryToPipeline(query) {
     pipeline.push({ $sort: query.options.sort });
   }
 
-  pipeline.push({ $project: projection });
+  // pipeline.push({ $project: projection });
+  // FIXME  cannot exclude fields until mongo 3.4
   return pipeline;
 }
 
@@ -92,29 +84,34 @@ function allOrganizations(req, res, next) {
   const pipeline = queryToPipeline(query);
 
   organizations.aggregate(pipeline).then(docs => {
-    const data = orgDataMap(docs);
-
-    const PP = data.map(o => {
-      const regex = new RegExp(o.names.join('|'), 'i');
-
-      return new Promise((resolve, reject) => {
-        contracts.find({ dependency: regex }, '-user_id').then(c => {
-          o.contracts = c;
-          resolve(o);
-        });
-      });
-    });
-
-    Promise.all(PP).then(values => {
       res.charSet('utf-8');
       res.json({
         status: 'success',
-        data: values,
-        size: values.length,
+        data: docs.map(o => (orgDataMap(o))),
+        size: docs.length,
       });
     });
 
-  });
+    // FIXME disalble linking dependency util we can get it in aggretation
+    // const PP = docs.map(o => {
+    //   const regex = new RegExp(o.names.join('|'), 'i');
+
+    //   return new Promise((resolve, reject) => {
+    //     contracts.find({ dependency: regex }, '-user_id').then(c => {
+    //       o.contracts = c;
+    //       resolve(o);
+    //     });
+    //   });
+    // });
+
+    // Promise.all(PP).then(values => {
+    //   res.charSet('utf-8');
+    //   res.json({
+    //     status: 'success',
+    //     data: orgDataMap(values),
+    //     size: values.length,
+    //   });
+    // });
 }
 
 function singleOrganization(req, res) {
@@ -135,7 +132,7 @@ function singleOrganization(req, res) {
         from: 'memberships',
         localField: 'simple',
         foreignField: 'org_id',
-        as: 'memberships_org',
+        as: 'shares',
       },
     },
     {
@@ -146,28 +143,12 @@ function singleOrganization(req, res) {
         as: 'memberships_sob',
       },
     },
-    { $project: defaultProjection },
   ]).then(docs => {
-    const data = orgDataMap(docs);
-
-    const PP = data.map(o => {
-      const regex = new RegExp(o.names.join('|'), 'i');
-
-      return new Promise((resolve, reject) => {
-        contracts.find({ dependency: regex }, '-user_id').then(c => {
-          o.contracts = c;
-          resolve(o);
-        });
-      });
-    });
-
-    Promise.all(PP).then(values => {
-      res.charSet('utf-8');
-      res.json({
-        status: 'success',
-        data: values,
-        size: values.length,
-      });
+    res.charSet('utf-8');
+    res.json({
+      status: 'success',
+      data: docs.map(o => (orgDataMap(o))),
+      size: docs.length,
     });
   });
 }
