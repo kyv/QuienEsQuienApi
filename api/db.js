11c73e13507552639fa8d920bbd651fc75c85adc
Timothy Kevin Brown <kevin@ki-ai.org>
2017-05-17 17:37:11 -0500

    simplify dbConnect

--- api/db.js
+++ api/db.js
@@ -1,11 +1,8 @@
 const monk = require('monk');
 
 const defaultDB = 'localhost:27017/meteor';
-let dbConnect = process.env.MONGODB_URI;
+const dbConnect = process.env.MONGODB_URI || defaultDB;
 
-if (!dbConnect) {
-  dbConnect = defaultDB;
-}
 const db = monk(dbConnect);
 
 db.then(() => {
