a51bc24a3c286561a26c3e5118ca3b829456c257
Timothy Kevin Brown <kevin@ki-ai.org>
2017-06-06 17:37:00 -0500

    export app for tests

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const SwaggerRestify = require('swagger-restify-mw');
 const restify = require('restify');
 const app = restify.createServer();
 
+module.exports = app; // for testing
+
 const config = {
   appRoot: __dirname, // required config
 };
