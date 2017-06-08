e0f6230ed6f0263a62d40d6bcd0a27e4518548d6
Timothy Kevin Brown <kevin@ki-ai.org>
2017-06-08 17:32:25 -0500

    add post actions to controllers

--- api/controllers/organizations.js
+++ api/controllers/organizations.js
@@ -1,5 +1,5 @@
 const db = require('../db');
-const organizations = db.get('organizations', { castIds: false });
+const collection = db.get('organizations', { castIds: false });
 const omit = require('lodash/omit');
 const omitEmpty = require('./lib').omitEmpty;
 const queryToPipeline = require('./lib').queryToPipeline;
@@ -59,7 +59,7 @@ function allOrganizations(req, res) {
   const query = getQuery(req);
 
   res.charSet('utf-8');
-  allDocuments(query, organizations, JOINS)
+  allDocuments(query, collection, JOINS)
   .then(array => {
     let data = array[1];
     const size = array[1].length;
@@ -78,11 +78,33 @@ function allOrganizations(req, res) {
   });
 }
 
+function allOrganizationsPost(req, res) {
+  const query = req.body.query;
+  const project = req.body.project;
+  const resultsP = collection.find(query);
+  const countP = collection.count(query);
+
+  res.charSet('utf-8');
+  return Promise.all([countP, resultsP])
+  .then(array => {
+    const data = array[1];
+    const size = data.length;
+
+    res.json({
+      status: 'success',
+      data,
+      size,
+      offset: project && project.limit || 0,
+      pages: Math.ceil((array[0] / size)),
+    });
+  });
+}
+
 function singleOrganization(req, res) {
   const query = getQuery(req);
   const pipeline = queryToPipeline(query, JOINS);
 
-  organizations.aggregate(pipeline).then(docs => {
+  collection.aggregate(pipeline).then(docs => {
     res.charSet('utf-8');
     res.json({
       status: 'success',
@@ -95,4 +117,5 @@ function singleOrganization(req, res) {
 module.exports = {
   allOrganizations,
   singleOrganization,
+  allOrganizationsPost,
 };
