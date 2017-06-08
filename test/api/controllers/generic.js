554b6766484094991e9dde74e6b86131a4b9e023
Timothy Kevin Brown <kevin@ki-ai.org>
2017-06-08 17:32:54 -0500

    update tests
      * add tests for post methods
      * fix style
      * move post tests to their own loop

--- test/api/controllers/generic.js
+++ test/api/controllers/generic.js
@@ -28,16 +28,16 @@ describe('controllers', function() {
 
     describe(cName, function() {
 
+      before(function(done) {
+        const mapped = testData.map(o => (extend(o, {created_at: new Date('2016-06-10')})));
+        collection.insert(mapped).then(() => done());
+      });
+
+      after(function(done) {
+        collection.drop(() => (done()))
+      });
+
       describe(`GET ${PATH}`, function() {
-
-        before(function(done) {
-          const mapped = testData.map(o => (extend(o, {created_at: new Date('2016-06-10')})));
-          collection.insert(mapped).then((docs) => done());
-        });
-
-        after(function(done) {
-          collection.drop(() => (done()))
-        });
 
         it(`should return 5 ${cName}`, function(done) {
 
@@ -120,13 +120,15 @@ describe('controllers', function() {
           });
         });
 
-        it(`should accept range query param`, function(done) {
+        it('should accept range query param', function(done) {
           const dateString = '2016-06-6';
           const dateObject = new Date(dateString);
-          collection.find({ created_at: { $gt: dateObject }})
-          .then((docs) => {
+
+          collection.find({ created_at: { $gt: dateObject } })
+          .then(docs => {
             const ids = docs.map(o => (o._id));
             const path = `${PATH}?created_at>${dateString}`;
+
             request(server)
               .get(path)
               .set('Accept', 'application/json')
@@ -141,10 +143,53 @@ describe('controllers', function() {
               });
           });
         });
+      });
 
+      describe(`POST ${PATH}`, function() {
+
+        it('should post query', function(done) {
+
+          collection.findOne()
+          .then(doc => {
+            const id = doc._id;
+            request(server)
+              .post(PATH)
+              .send({ query: { _id: id } })
+              .set('Accept', 'application/json')
+              .expect('Content-Type', /json/)
+              .expect(200)
+              .end(function(err, res) {
+                should.not.exist(err);
+                res.body.status.should.eql('success');
+                res.body.data.map(o => o._id).should.eql([id]);
+
+                done();
+              });
+          });
+        });
+
+      it('should post query multiple', function(done) {
+        collection.find()
+        .then(docs => {
+          const ids = docs.map(o => (o._id));
+
+          request(server)
+            .post(PATH)
+            .send({ query: { _id: { $in: ids } } })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function(err, res) {
+              should.not.exist(err);
+              res.body.status.should.eql('success');
+              res.body.data.map(o => o._id).should.eql(ids
+              );
+
+              done();
+            });
+        });
+      });
       });
     });
-
   }
-
 });
