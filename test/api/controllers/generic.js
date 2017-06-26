811c814fcce90df6dd60c57b68457c7a3de3cda5
kevin <kevin@rindecuentas.org>
2017-06-26 11:08:21 -0500

    follow the linter generic tests

--- test/api/controllers/generic.js
+++ test/api/controllers/generic.js
@@ -1,3 +1,4 @@
+/* eslint-env mocha */
 const should = require('should');
 const request = require('supertest');
 const server = require('../../../app');
@@ -18,183 +19,179 @@ const COLLECTIONS = [
   organizations,
 ];
 
-describe('controllers', function() {
+function addDatesToData(object) {
+  return extend(object, {
+    created_at: new Date('2016-06-10'),
+  });
+}
 
-  for (let i = 0; i < COLLECTIONS.length; i++) {
-    const collection = COLLECTIONS[i];
-    const cName = collection.name;
-    const PATH = `/v1/${cName}`;
-    const testData = testDataJson[cName];
+function collectionRutine(testData, collection, PATH, cName) {
+  before(done => {
+    const mapped = testData.map(o => (addDatesToData(o)));
 
-    describe(cName, function() {
+    collection.insert(mapped).then(() => done());
+  });
 
-      before(function(done) {
-        const mapped = testData.map(o => (extend(o, {created_at: new Date('2016-06-10')})));
-        collection.insert(mapped).then(() => done());
-      });
+  after(done => (collection.drop(() => (done()))));
 
-      after(function(done) {
-        collection.drop(() => (done()))
-      });
+  describe(`GET ${PATH}`, () => {
 
-      describe(`GET ${PATH}`, function() {
+    it(`should return distinct values ${cName}`, done => {
+      const path = `${PATH}/distinct/source`;
 
-        it(`should return distinct values ${cName}`, function(done) {
-          const path = `${PATH}/distinct/source`;
+      collection.distinct('source').then(docs => {
+        request(server)
+          .get(path)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.body.status.should.eql('success');
+            res.body.data.should.not.eql([]);
+            res.body.data.length.should.eql(docs.length);
 
-          collection.distinct('source').then(docs => {
-            request(server)
-              .get(path)
-              .set('Accept', 'application/json')
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                should.not.exist(err);
-                res.body.status.should.eql('success');
-                res.body.data.should.not.eql([]);
-                res.body.data.length.should.eql(docs.length);
-
-                done();
-              });
+            done();
           });
+      });
 
+    });
+
+
+    it(`should return 5 ${cName}`, done => {
+
+      request(server)
+        .get(PATH)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          should.not.exist(err);
+          res.body.status.should.eql('success');
+          res.body.data.length.should.eql(5);
+
+          done();
         });
+    });
 
+    it('should accept a path parameter', done => {
 
-        it(`should return 5 ${cName}`, function(done) {
+      collection.findOne().then(doc => {
+        const id = doc._id;
+        const path = `${PATH}/${id}`;
+
+        request(server)
+          .get(path)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.body.status.should.eql('success');
+            res.body.data[0]._id.should.eql(id);
+
+            done();
+          });
+      });
+    });
+
+    it('should accept query param', done => {
+
+      collection.findOne().then(doc => {
+        const id = doc._id;
+
+        request(server)
+          .get(PATH)
+          .query({ _id: id })
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.body.status.should.eql('success');
+            res.body.data[0]._id.should.eql(id);
+
+            done();
+          });
+      });
+    });
+
+    it('should accept multiple comma sepereated query param', done => {
+
+      collection.find({}, { limit: 2 }).then(docs => {
+        const ids = docs.map(o => (o._id));
+        const CSV = ids.join(',');
+        const path = `${PATH}?_id=${CSV}`;
+
+        request(server)
+          .get(path)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .end((err, res) => {
+            should.not.exist(err);
+            res.body.status.should.eql('success');
+            res.body.data.should.not.eql([]);
+            res.body.data.map(o => o._id).should.eql(ids);
+
+            done();
+          });
+      });
+    });
+
+    it('should accept range query param', done => {
+      const dateString = '2016-06-6';
+      const dateObject = new Date(dateString);
+
+      collection.find({ created_at: { $gt: dateObject } })
+        .then(docs => {
+          const ids = docs.map(o => (o._id));
+          const path = `${PATH}?created_at>${dateString}`;
 
           request(server)
-            .get(PATH)
+            .get(path)
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
-            .end(function(err, res) {
+            .end((err, res) => {
               should.not.exist(err);
               res.body.status.should.eql('success');
-              res.body.data.length.should.eql(5);
+              res.body.data.should.not.eql([]);
+              res.body.data.map(o => o._id).should.eql(ids);
 
               done();
             });
         });
+    });
+  });
 
-        it('should accept a path parameter', function(done) {
+  describe(`POST ${PATH}`, () => {
 
-          collection.findOne().then((doc) => {
-            const id = doc._id;
-            const path = `${PATH}/${id}`;
+    it('should post query', done => {
 
-            request(server)
-              .get(path)
-              .set('Accept', 'application/json')
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                should.not.exist(err);
-                res.body.status.should.eql('success');
-                res.body.data[0]._id.should.eql(id);
+      collection.findOne()
+        .then(doc => {
+          const id = doc._id;
 
-                done();
-              });
-          });
+          request(server)
+            .post(PATH)
+            .send({ query: { _id: id } })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+              should.not.exist(err);
+              res.body.status.should.eql('success');
+              res.body.data.should.not.eql([]);
+              res.body.data.map(o => o._id).should.eql([id]);
+
+              done();
+            });
         });
+    });
 
-        it(`should accept query param`, function(done) {
-
-          collection.findOne().then((doc) => {
-            const id = doc._id;
-            const path = `${PATH}/${id}`;
-
-            request(server)
-              .get(PATH)
-              .query({ _id: id })
-              .set('Accept', 'application/json')
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                should.not.exist(err);
-                res.body.status.should.eql('success');
-                res.body.data[0]._id.should.eql(id);
-
-                done();
-              });
-          });
-        });
-
-        it(`should accept multiple comma sepereated query param`, function(done) {
-
-          collection.find({}, {limit: 2}).then((docs) => {
-            const ids = docs.map(o => (o._id));
-            const CSV = ids.join(',');
-            const path = `${PATH}?_id=${CSV}`;
-
-            request(server)
-              .get(path)
-              .set('Accept', 'application/json')
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                should.not.exist(err);
-                res.body.status.should.eql('success');
-                res.body.data.should.not.eql([]);
-                res.body.data.map(o => o._id).should.eql(ids);
-
-                done();
-              });
-          });
-        });
-
-        it('should accept range query param', function(done) {
-          const dateString = '2016-06-6';
-          const dateObject = new Date(dateString);
-
-          collection.find({ created_at: { $gt: dateObject } })
-          .then(docs => {
-            const ids = docs.map(o => (o._id));
-            const path = `${PATH}?created_at>${dateString}`;
-
-            request(server)
-              .get(path)
-              .set('Accept', 'application/json')
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                should.not.exist(err);
-                res.body.status.should.eql('success');
-                res.body.data.should.not.eql([]);
-                res.body.data.map(o => o._id).should.eql(ids);
-
-                done();
-              });
-          });
-        });
-      });
-
-      describe(`POST ${PATH}`, function() {
-
-        it('should post query', function(done) {
-
-          collection.findOne()
-          .then(doc => {
-            const id = doc._id;
-            request(server)
-              .post(PATH)
-              .send({ query: { _id: id } })
-              .set('Accept', 'application/json')
-              .expect('Content-Type', /json/)
-              .expect(200)
-              .end(function(err, res) {
-                should.not.exist(err);
-                res.body.status.should.eql('success');
-                res.body.data.should.not.eql([]);
-                res.body.data.map(o => o._id).should.eql([id]);
-
-                done();
-              });
-          });
-        });
-
-      it('should post query multiple', function(done) {
-        collection.find()
+    it('should post query multiple', done => {
+      collection.find()
         .then(docs => {
           const ids = docs.map(o => (o._id));
 
@@ -204,7 +201,7 @@ describe('controllers', function() {
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
-            .end(function(err, res) {
+            .end((err, res) => {
               should.not.exist(err);
               res.body.status.should.eql('success');
               res.body.data.should.not.eql([]);
@@ -214,8 +211,19 @@ describe('controllers', function() {
               done();
             });
         });
-      });
-      });
     });
+  });
+
+}
+
+describe('controllers', () => {
+
+  for (let i = 0; i < COLLECTIONS.length; i++) {
+    const collection = COLLECTIONS[i];
+    const cName = collection.name;
+    const PATH = `/v1/${cName}`;
+    const testData = testDataJson[cName];
+
+    describe(cName, () => collectionRutine(testData, collection, PATH, cName));
   }
 });
